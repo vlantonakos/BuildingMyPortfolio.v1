0ddb236d22b819307cfe8c6c8bae7a59161b283b
Antonis <107072477+vlantonakos@users.noreply.github.com>
2023-05-05 20:06:29 +0300

    Add files via upload

---
+++ portfolio/src/routes/portfolio.jsx
@@ -0,0 +1,12 @@
+import NavBar from "../components/NavBar";
+import '../styles/navbar.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+export default function Portfolio() {
+    return (
+        <div className="App">
+            <NavBar />
+        </div>
+    )
+}
